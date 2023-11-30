d4ed7cd96d7f711765458e27de63047eb3737825
Tales <talesdearaujo18@gmail.com>
2023-11-30 16:13:29 -0300

    mas um

--- Pog Dev/Program.cs
+++ Pog Dev/Program.cs
@@ -1,50 +1,70 @@
-﻿public class Advogado
-{
-    public string Nome { get; private set; }
-    public DateTime DataNasc { get; private set; }
-    public string Cpf { get; private set; }
-    public string Cna { get; private set; }
+﻿using System;
+using System.Collections.Generic;
 
-    public Advogado(string nome, DateTime dataNasc, string cpf, string cna)
+class Program
+{
+    static void Main()
     {
-        Nome = nome;
-        DataNasc = dataNasc;
-        Cpf = cpf;
-        Cna = cna;
+        Escritorio escritorio = new Escritorio("Escritório de Advocacia XYZ");
+
+        Advogado advogado1 = new Advogado("Tales", new DateTime(1976, 2, 1), "12345678901", "CNA-123");
+        Advogado advogado2 = new Advogado("Danilo", new DateTime(1976, 3, 16), "98765432109", "CNA-456");
+
+        escritorio.AdicionarAdvogado(advogado1);
+        escritorio.AdicionarAdvogado(advogado2);
+
+        Cliente cliente1 = new Cliente("Cliente 1", new DateTime(1985, 3, 10), "98765432101", "Casado", "Médico");
+        Cliente cliente2 = new Cliente("Cliente 2", new DateTime(1992, 7, 20), "12345678902", "Solteiro", "Fisioteraupeta");
+
+        escritorio.AdicionarCliente(cliente1);
+        escritorio.AdicionarCliente(cliente2);
+
+        
+        Console.WriteLine($"Nome do Escritório: {escritorio.Nome}");
+        Console.WriteLine("\nAdvogados:");
+        foreach (Advogado advogado in escritorio.Advogados)
+        {
+            Console.WriteLine($"Nome: {advogado.Nome}");
+            Console.WriteLine($"Data de Nascimento: {advogado.DataNascimento.ToShortDateString()}");
+            Console.WriteLine($"CPF: {advogado.CPF}");
+            Console.WriteLine($"CNA: {advogado.CNA}");
+            Console.WriteLine("\n");
+        }
+
+        Console.WriteLine("\nClientes:");
+        foreach (Cliente cliente in escritorio.Clientes)
+        {
+            Console.WriteLine($"Nome: {cliente.Nome}");
+            Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento.ToShortDateString()}");
+            Console.WriteLine($"CPF: {cliente.CPF}");
+            Console.WriteLine($"Estado Civil: {cliente.EstadoCivil}");
+            Console.WriteLine($"Profissão: {cliente.Profissao}");
+            Console.WriteLine("\n");
+        }
+
+        // Gerando relatórios
+        Console.WriteLine("Relatórios:");
+        Console.WriteLine($"Número de Advogados: {escritorio.ObterNumeroAdvogados()}");
+        Console.WriteLine($"Número de Clientes: {escritorio.ObterNumeroClientes()}");
     }
 }
-public class Cliente
+
+class Escritorio
 {
     public string Nome { get; private set; }
-    public DateTime DataNasc { get; private set; }
-    public string Cpf { get; private set; }
-    public string EstadoCivil { get; private set; }
-    public string Profissao { get; private set; }
+    public List<Advogado> Advogados { get; private set; }
+    public List<Cliente> Clientes { get; private set; }
 
-    public Cliente(string nome, DateTime dataNasc, string cpf, string estadoCivil, string profissao)
+    public Escritorio(string nome)
     {
         Nome = nome;
-        DataNasc = dataNasc;
-        Cpf = cpf;
-        EstadoCivil = estadoCivil;
-        Profissao = profissao;
+        Advogados = new List<Advogado>();
+        Clientes = new List<Cliente>();
     }
-}
-    ExibirInformacoes(advogado, cliente);
-    
 
-    public static void ExibirInformacoes(Advogado advogado, Cliente cliente)
+    public void AdicionarAdvogado(Advogado advogado)
     {
-        Console.WriteLine("Advogado:");
-        Console.WriteLine($"Nome: {advogado.Nome}");
-        Console.WriteLine($"Data de Nascimento: {advogado.DataNasc.ToShortDateString()}");
-        Console.WriteLine($"CPF: {advogado.Cpf}");
-        Console.WriteLine($"CNA: {advogado.Cna}");
-
-        Console.WriteLine("\nCliente:");
-        Console.WriteLine($"Nome: {cliente.Nome}");
-        Console.WriteLine($"Data de Nascimento: {cliente.DataNasc.ToShortDateString()}");
-        Console.WriteLine($"CPF: {cliente.Cpf}");
-        Console.WriteLine($"Estado Civil: {cliente.EstadoCivil}");
-        Console.WriteLine($"Profissão: {cliente.Profissao}");
+        Advogados.Add(advogado);
     }
+
+   
